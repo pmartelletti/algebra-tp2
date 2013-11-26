178a2b97593dd9492562e6f5a3591abbca3278ba
Pablo Martelletti <pmartelletti@gmail.com>
2013-11-26 16:42:20 -0300

    Modifico ejercicio2 por error de interpretacion del enunciado.

--- funciones.hs
+++ funciones.hs
@@ -19,9 +19,17 @@ esMultiplo17 :: [Int] -> Bool
 esMultiplo17 [x] = False
 esMultiplo17 (x:xs) = ((obtenerUltimo xs) * 5 - sumaList (x:(eliminarUltimo xs))) `mod` 17 == 0
 
+potencia:: (Int, Int) -> Int
+potencia (b, 0) = 1
+potencia (b, n) = b * potencia (b,(n-1))
+
+formarEnBase10 :: [Int] -> Int
+formarEnBase10 [x] = x
+formarEnBase10 (x:xs) = x * potencia(10,long(xs)) + formarEnBase10 xs
+
 --- ejercicio 2
 esMultiplo19 :: [Int] -> Bool
 esMultiplo19 [x] = False
-esMultiplo19 (x:xs) = ((obtenerUltimo xs) * 2 + sumaList (x:(eliminarUltimo xs))) `mod` 19 == 0
+esMultiplo19 (x:xs) = ((obtenerUltimo xs) * 2 + formarEnBase10 (x:eliminarUltimo xs)) `mod` 19 == 0
 
 -- ejercicio 3
