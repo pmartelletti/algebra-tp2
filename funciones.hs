34175a5c584159651ee4850f9bc6bf18c3eea278
Pablo Martelletti <pmartelletti@gmail.com>
2013-11-26 16:44:59 -0300

    Agrego un poco de documentacion.

--- funciones.hs
+++ funciones.hs
@@ -1,15 +1,19 @@
+--- longitud de una lista
 long :: [a] -> Int
 long [] = 0
 long (x:xs) = 1 + (long xs)
 
+--- suma total de los elementos de una lista de enteros
 sumaList :: [Int] -> Int
 sumaList [] = 0
 sumaList (x:xs) = x + (sumaList xs)
 
+--- elimina el ultimo elemento de una lista de enteros
 eliminarUltimo :: [Int] -> [Int]
 eliminarUltimo [x] = []
 eliminarUltimo (x:xs) = x : eliminarUltimo xs
 
+--- obtengo el ultimo elemento de una lista de enteros
 obtenerUltimo :: [Int] -> Int
 obtenerUltimo [] = 0
 obtenerUltimo x = (x!!(long(x)-1))
@@ -23,6 +27,8 @@ potencia:: (Int, Int) -> Int
 potencia (b, 0) = 1
 potencia (b, n) = b * potencia (b,(n-1))
 
+--- formo un numero en base 10, dados sus elementos en unidades, decenas, etc.
+--- ej: formarEnBase10 [2,8,3] devuelve 283
 formarEnBase10 :: [Int] -> Int
 formarEnBase10 [x] = x
 formarEnBase10 (x:xs) = x * potencia(10,long(xs)) + formarEnBase10 xs
